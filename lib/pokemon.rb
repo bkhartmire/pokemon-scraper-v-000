2dedcb45f4b2b7784010d366c4c210ea58d25b0f
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-08 06:20:42 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,17 +1,20 @@
 class Pokemon
   @@all = []
-  
+
   def initialize(name, type, db)
     @name = name
     @type = type
     @db = db
+    @@all << self
   end
 
   def self.all
     @@all
   end
-  
-  def self.save
+
+  def self.save(name, type, db)
+    db
+    
   end
 
   def self.find
