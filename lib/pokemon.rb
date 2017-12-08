becc891d7a0b65a05f569412642a82cd07ea20fc
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-08 06:37:15 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   @@all = []
   attr_accessor :name, :type, :db
-  def initialize(name, type, db)
+  def initialize(name:, type:, db:)
     @name = name
     @type = type
     @db = db
